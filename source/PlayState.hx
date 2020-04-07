7aa68b5a03b5860c25a3960f50fe8c90f402491c
LunarBitz <atmosphir44@gmail.com>
2020-04-06 23:11:20 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,34 @@
 package;
 
+import flixel.math.FlxMath;
 import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.math.FlxAngle;
+import flixel.math.FlxMath;
 
 class PlayState extends FlxState
 {
+	var text = new FlxText();
+	var dy:Float = 0;
+	var dt:Float = 0;
+
 	override public function create():Void
 	{
 		super.create();
+
+		text.text = "Hello World!!";
+		text.size = 64;
+		text.screenCenter();
+		text.moves = true;
+		add(text);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		
+		dt += elapsed;
+		dy = FlxMath.fastSin(dt * 2.5) * 4.0;
+		text.setPosition(text.x, text.y + dy);
 	}
 }
