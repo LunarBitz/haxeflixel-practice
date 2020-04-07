6596d59fd4988caa3483e4d934dfd892a0baabdc
LunarBitz <atmosphir44@gmail.com>
2020-04-07 17:38:30 -0500

    Hero Fix + Playstate Fix
    - Completed Part 2 and Bonus
    ^ Hero Position Wrapping
    ^ Keyboard Input)
    
    - Cleaned up PlayState.hx
    ^ No longer initializing outside of create method

--- source/Hero.hx
+++ source/Hero.hx
@@ -1,23 +1,93 @@
-import flixel.math.FlxRandom;
-import lime.math.ColorMatrix;
+package;
+
 import flixel.util.FlxColor;
 import flixel.FlxSprite;
+import flixel.FlxG;
+import flixel.util.FlxTimer;
 
 class Hero extends FlxSprite
 { 
-    var randomGen:FlxRandom = new FlxRandom();
+    public var xSpeed:Float = 0.0;
+    public var ySpeed:Float = 0.0;
+    public var targetSpeed:Float = 75.0;
+
+    private var keyLeft:Int = 0;
+    private var keyRight:Int = 0;
+    private var keyUp:Int = 0;
+    private var keyDown:Int = 0;
+
+    private var overStimulationProtection:Bool = true;
 
 	override public function new(x:Float = 0.0, y:Float = 0.0)
     {
         super(x, y);
-        makeGraphic(64, 64, 0xff00ff00);
+
+        makeGraphic(32, 32);
+
+        if (overStimulationProtection)
+        {
+            new FlxTimer().start(0.5, overStimulationCallback, 0);
+        }
     }
 
     override public function update(elapsed:Float)
     {
         super.update(elapsed);
+        
+        updateMovement();
 
-        var randomColor:FlxColor = randomGen.color(0x00000000, 0xFFFFFFFF);
+        if (!overStimulationProtection)
+        {
+            // Call visual methods that may trigger epilepsy
+            randomizeColor();
+        }
+    }
+
+    public function updateMovement():Void
+    {
+        // Set keyboard booleans to integers
+        keyLeft = (FlxG.keys.pressed.A)? 1:0;
+        keyRight = (FlxG.keys.pressed.D)? 1:0;
+        keyUp = (FlxG.keys.pressed.W)? 1:0;
+        keyDown = (FlxG.keys.pressed.S)? 1:0;
+
+        // Set keyboard integers as scaled axis
+        this.xSpeed = (keyRight - keyLeft) * targetSpeed;   // Same as setting using if statements while letting "keyLeft" return -1
+        this.ySpeed = (keyDown - keyUp) * targetSpeed;  // Same as setting using if statements while letting "keyUp" return -1
+
+        // Update velocity
+        this.velocity.set(xSpeed, ySpeed);  
+
+        // Wrap hero's position horizontally
+        if (this.x > FlxG.camera.width)
+        {
+            this.x = 0;
+        }
+        else if (this.x < 0)
+        {
+            this.x = FlxG.camera.width;
+        }
+
+        // Wrap hero's position vertically
+        if (this.y > FlxG.camera.height)
+        {
+            this.y = 0;
+        }
+        else if (this.y < 0)
+        {
+            this.y = FlxG.camera.height;
+        }  
+    }
+
+    function overStimulationCallback(timer:FlxTimer):Void
+    {
+        // Call visual methods that may trigger epilepsy
+        randomizeColor();
+    }
+
+    public function randomizeColor():Void
+    {
+        var randomColor:FlxColor = FlxG.random.color(0xFF000000, 0xFFFFFFFF);
         this.color = randomColor;
     }
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6,32 +6,34 @@ import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
-	var text = new FlxText();
-	var player:Hero = new Hero();
+	var text:FlxText;
+	var player:Hero;
+	
+	var totalTime:Float = 0;
 	var dy:Float = 0;
-	var dt:Float = 0;
 
 	override public function create():Void
 	{
 		super.create();
 
+		player = new Hero(25.0, 25.0);
+		add(player);
+
+		text = new FlxText();
 		text.text = "Hello World!!";
 		text.size = 64;
 		text.screenCenter();
 		text.moves = true;
 		add(text);
-
-		player = new Hero(25.0, 25.0);
-		//add(player);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
+		// elapsed IS deltaTime
 		super.update(elapsed);
 		
-		dt += elapsed;
-		dy = FlxMath.fastSin(dt * 2.5) * 256.0;
-		//text.setPosition(text.x, text.y + dy);
+		totalTime += elapsed;
+		dy = FlxMath.fastSin(totalTime * 2.5) * 256.0;
 		text.velocity.set(0, dy);
 	}
 }
